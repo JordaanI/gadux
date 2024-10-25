ef20ba92cb86fad48027010217530a41543d3006
Ivan <jordaanivan@gmail.com>
2024-10-25 16:08:06 -0400

    removed tests

--- lib/main.scm
+++ lib/main.scm
@@ -39,29 +39,3 @@
 
 (define-macro (execute s)
   `(raw-execute state ,s))
-
-
-;;;;
-;;;; TESTS
-;;;;
-
-(define (state-loop state)
-  (let ((sum (raw-execute state 'one)))
-    (println "Sum is now " sum)
-    (if (= 5 sum) (state-loop (comp-datum-set! state 'one (list (cons 'one (execute 'two)))))
-        (state-loop (state-data-set! state (cons 'one (list (cons 'one sum))))))))
-
-
-(define-executable (add one)
-  (thread-sleep! 1)
-  (display "Working...")
-  (newline)
-  (+ one 1))
-
-(define-executable (reset two)
-  (println "In add2..")
-  two)
-
-(state-loop
- (state-add-all! state (make-executable-comp 'one add (cons 'one 1)) (make-executable-comp 'two reset (cons 'two 1)))
- )
